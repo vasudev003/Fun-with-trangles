f27ada32d034b79c39c50305f800ae13d08ab10d
vasudev003 <vasudevpeshwani.vp@gmail.com>
2022-01-22 11:52:08 +0530

    initial commit

---
+++ area.js
@@ -0,0 +1,9 @@
+const 
+const checkAreaBtn = document.querySelector("#chck-Area-Btn");
+const outputEl = document.querySelector("#output");
+
+function AreaOFaTriangle (){
+
+}
+
+checkAreaBtn.addEventListener('click', AreaOFaTriangle )
